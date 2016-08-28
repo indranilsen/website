57a4f7292736c7e89a9b42229f89d7db72dd294e
Indranil Sen <indranilsen010@gmail.com>
2016-08-28 17:50:21 -0400

    Styled Projects section

--- web/js/directives/projects.js
+++ web/js/directives/projects.js
@@ -6,11 +6,11 @@ angular.module('app')
 
 			$scope.personalProjects = [
 				{
-					name: "",
-					description: "",
-					imageSrc: "",
-					imageAlt: "",
-					tags: ""
+					name: "ParkSafe",
+					description: "This project was inspired by the prevalent issue of bike theft. ParkSafe allows users to either text-message their address or enter it on our Web App. The ParkSafe API then returns 5 bike racks closest to the user with different risk scores: a confidence percentage of the bike being stolen.Risk scores are based from crime data collected from publicly available records (City of Toronto, Toronto Police, etc.).",
+					imageSrc: "parksafe.jpg",
+					imageAlt: "A solution to the rampant issue of bike theft",
+					tags: "AngularJS, PHP, Laravel, MongoDB"
 				}
 			];
 
