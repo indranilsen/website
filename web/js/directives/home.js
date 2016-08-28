eff64fa88eb1671e0241feee7907192a61c033d0
Indranil Sen <indranilsen010@gmail.com>
2016-08-28 00:36:47 -0400

    Added greeting function

--- web/js/directives/home.js
+++ web/js/directives/home.js
@@ -2,8 +2,35 @@ angular.module('app')
     .directive('indranilSenMainHome', function() {
         return {
             templateUrl: 'partials/main-content/home.html',
-            controller: function($scope, $rootScope) {
+            controller: function($scope, $rootScope, $interval) {
                 $scope.greeting = "Hello!";
+
+								var numLast1 = 0;
+								var numLast2 = 5;
+                function alternateGreeting() {
+                    var greetings = [
+                        "Hello",
+                        "Bonjour",
+                        "Namaste",
+                        "Hola",
+                        "Ciao",
+                        "Hallo",
+                        "Kon'nichiwa",
+                        "Nǐ hǎo",
+                        "Annyeong"
+                    ];
+                    var num = Math.floor(Math.random() * (greetings.length));
+										numLast1 = numLast2;
+										numLast2 = num;
+
+										while (num == numLast1 || num == numLast2) {
+											num = Math.floor(Math.random() * (greetings.length));
+										}
+
+                    $scope.greeting = greetings[num] + '!';
+                }
+
+                $interval(alternateGreeting, 3000);
             }
         };
     });
