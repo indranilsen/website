7fde99a0b94f68596a84075dec3b290778c3bd98
Indranil Sen <indranilsen010@gmail.com>
2016-08-28 17:21:23 -0400

    Dynamically adding projects from controller

--- web/js/directives/projects.js
+++ web/js/directives/projects.js
@@ -1,6 +1,19 @@
 angular.module('app')
 .directive('indranilSenMainProjects', function() {
 	return {
-		templateUrl: 'partials/main-content/projects.html'
+		templateUrl: 'partials/main-content/projects.html',
+		controller: function($scope, $rootScope) {
+
+			$scope.personalProjects = [
+				{
+					name: "",
+					description: "",
+					imageSrc: "",
+					imageAlt: "",
+					tags: ""
+				}
+			];
+
+		}
 	};
 });
