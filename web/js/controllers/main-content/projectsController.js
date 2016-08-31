b7ea1d67e32e36484b425b5bd3689cb35bc358d5
Indranil Sen <indranilsen010@gmail.com>
2016-08-31 16:06:49 -0400

    Project section style fixed

--- web/js/controllers/main-content/projectsController.js
+++ web/js/controllers/main-content/projectsController.js
@@ -2,6 +2,6 @@ angular.module('app')
     .controller('projectsController', function($scope, $stateParams, $state) {
         console.log("Hello from projectsController");
 				var name = $stateParams.name;
-        $scope.state = $state.current
+        $scope.state = $state.current;
         $scope.params = $stateParams;
     });
