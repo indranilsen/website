2e74e0ccab8aac37a2b4b643e3112e572a1a27f4
Indranil Sen <indranilsen010@gmail.com>
2016-09-25 17:23:47 -0400

    Conent update for projects seciton

--- web/js/providers/services/projectsHelper.js
+++ web/js/providers/services/projectsHelper.js
@@ -4,7 +4,7 @@ angular.module('app')
       {
         name: "ParkSafe",
         description: "This project was inspired by the prevalent issue of bike theft. ParkSafe allows users to either text-message their address or enter it on our Web App. The ParkSafe API then returns 5 bike racks closest to the user with different risk scores: a confidence percentage of the bike being stolen. Risk scores are based from crime data collected from publicly available records (City of Toronto, Toronto Police, etc.).",
-        info: "A solution to the rampant issue of bike theft",
+        info: "A solution to the rampant issue of bike theft; 1st Place in the Microsoft Hackathon",
         image: "parkSafe.jpg",
         tags: "AngularJS, PHP, Laravel, MongoDB"
       },
