b87a7241f38343f42f70742908756962d7fe7c04
Indranil Sen <indranilsen010@gmail.com>
2017-02-22 23:50:10 -0500

    Forgot to add new files

---
+++ public/js/controllers/main-content/videosController.js
@@ -0,0 +1,5 @@
+angular.module('app')
+    .controller('videosController', function($scope) {
+        console.log("Hello from videosController");
+        $scope.query = '';
+    });
---
+++ public/js/directives/videos.js
@@ -0,0 +1,8 @@
+angular.module('app')
+    .directive('indranilSenMainVideos', function() {
+        return {
+    		templateUrl: 'partials/main-content/videos.html',
+    		controller: function($scope, $rootScope) {
+    		}
+    	};
+    });
