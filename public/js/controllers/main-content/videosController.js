5bae0c35e6209c01c85328b9d2c8b052acf9b5e9
Indranil Sen <indranilsen010@gmail.com>
2017-03-13 21:30:23 -0400

    Basic setup working

--- public/js/controllers/main-content/videosController.js
+++ public/js/controllers/main-content/videosController.js
@@ -1,5 +1,16 @@
 angular.module('app')
-    .controller('videosController', function($scope) {
+    .controller('videosController', function($scope, $http) {
         console.log("Hello from videosController");
+        var videoEndpoint = 'http://localhost:3000/api/video';
+
         $scope.query = '';
+
+        $http.get(videoEndpoint)
+            .then(function(res) {
+                console.log(res.data);
+                $scope.videos = res.data;
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
     });
