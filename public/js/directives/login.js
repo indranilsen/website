54c2d97cc4f6eeb8865559d462d835608bf0307a
Indranil Sen <indranilsen010@gmail.com>
2017-08-19 00:20:53 -0400

    Changed icon for register sections

--- public/js/directives/login.js
+++ public/js/directives/login.js
@@ -8,9 +8,21 @@ angular.module('app')
     				password: '',
     			};
 
+                $scope.registrant = {
+                    name: '',
+    				email: '',
+    				password: '',
+    			};
+
                 $scope.login = function(valid) {
     				if(valid) {
     					console.log("Email: ", $scope.member.email, "\nPassword: ", $scope.member.password);
+    				}
+    			};
+
+                $scope.register = function(valid) {
+    				if(valid) {
+    					console.log("Name: ", $scope.registrant.name, "\nEmail: ", $scope.registrant.email, "\nPassword: ", $scope.registrant.password);
     				}
     			};
     		}
