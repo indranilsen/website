63a53fb863129a64638b2572354e330847052c12
Indranil Sen <indranilsen010@gmail.com>
2017-08-21 20:50:17 -0400

    Fixed video button placement on Safari

--- public/js/directives/videos.js
+++ public/js/directives/videos.js
@@ -3,6 +3,18 @@ angular.module('app')
         return {
     		templateUrl: 'partials/main-content/videos.html',
     		controller: function($scope, $rootScope) {
+                angular.element(document.getElementsByClassName('video-cont')).ready(function(){
+                    angular.element(document.getElementsByClassName('button-elem')).ready(function() {
+                        var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+                        if(isSafari) {
+                            var buttons = document.getElementsByClassName('button-elem');
+                            for(var i = 0; i < buttons.length; i++) {
+                                buttons[i].style.marginTop = "73px";
+                                buttons[i].style.marginLeft = "135px";
+                            }
+                        }
+                    });
+                });
     		}
     	};
     });
